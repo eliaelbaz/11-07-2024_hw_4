049fedfd1842e1565391d7a6ace1ff25a6a931f6
eliaelbaz <eliaelbaz050@gmail.com>
2024-07-14 23:14:23 +0300

    Update hw_4_1_true.py

--- hw_4_1_true.py
+++ hw_4_1_true.py
@@ -1,77 +1,86 @@
-## start true
-
-a = 0
-b = 0
+## start - True
+a = 5
+b = 5
 if a == b:
     print (f"was True for {a} {b}");
 else:
     print (f"was False for {a} {b}");
-a = 1
-b = 1
-c = 1
-d = 1
+
+a = 3
+b = 4
+c = 2
+d = 5
 if a + b and c + d:
     print (f"was True for {a} {b} {c} {d}");
 else:
     print (f"was False for {a} {b} {c} {d}");
-a = 1
-b = 0
-c = 0
+
+a = 7
+b = 2
+c = 1
 d = 0
 if a >= b or c > d:
     print (f"was True for {a} {b} {c} {d}");
 else:
     print (f"was False for {a} {b} {c} {d}");
-a = 0
-b = 0
-c = 0
-d = 1
+
+a = 6
+b = 6
+c = 3
+d = 4
 if a >= b or c < d:
     print (f"was True for {a} {b} {c} {d}");
 else:
     print (f"was False for {a} {b} {c} {d}");
-a = 0
-b = 0
-c = 0
+
+a = 2
+b = 2
+c = 1
 d = 1
 if a == b and c <= d:
     print (f"was True for {a} {b} {c} {d}");
 else:
     print (f"was False for {a} {b} {c} {d}");
+
 a = 1
-b = 1
-c = 1
-d = 1
+b = 2
+c = 3
+d = 4
 if True and a + b + c + d:
     print (f"was True for {a} {b} {c} {d}");
 else:
     print (f"was False for {a} {b} {c} {d}");
-a = 1
-b = 0
+
+a = 8
+b = 3
 if a != b:
     print (f"was True for {a} {b}");
 else:
     print (f"was False for {a} {b}");
+
 a = 0
 b = 0
-c = 1
+c = 5
 if a != b and a <= c or a <= b or True:
     print (f"was True for {a} {b} {c}");
 else:
     print (f"was False for {a} {b} {c}");
+
 a = 0
 b = 0
-c = 1
+c = 7
 if a != b and a <= c or a <= b or False:
     print (f"was True for {a} {b} {c}");
 else:
     print (f"was False for {a} {b} {c}");
-a = 2
-b = 1
-c = 1
+
+a = 9
+b = 3
+c = 2
 d = 1
 if b != 0 and d != 0 and a % b == 0 and c % d == 0:
     print (f"was True for {a} {b} {c} {d}");
 else:
     print (f"was False for {a} {b} {c} {d}");
-##End
+
+## End

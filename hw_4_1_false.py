8b7210794ede82c15533d56b2cb84b2a7aad2a6b
eliaelbaz <eliaelbaz050@gmail.com>
2024-07-14 23:12:41 +0300

    Update hw_4_1_false.py

--- hw_4_1_false.py
+++ hw_4_1_false.py
@@ -1,1 +1,87 @@
+## Start False
+a = 8
+b = 3
+if a == b:
+    print (f"was True for {a} {b}");
+else:
+    print (f"was False for {a} {b}");
 
+a = 0
+b = 0
+c = 0
+d = 0
+if a + b and c + d:
+    print (f"was True for {a} {b} {c} {d}");
+else:
+    print (f"was False for {a} {b} {c} {d}");
+
+a = 4
+b = 5
+c = 2
+d = 3
+if a >= b or c > d:
+    print (f"was True for {a} {b} {c} {d}");
+else:
+    print (f"was False for {a} {b} {c} {d}");
+
+a = 3
+b = 3
+c = 7
+d = 6
+if a >= b or c < d:
+    print (f"was True for {a} {b} {c} {d}");
+else:
+    print (f"was False for {a} {b} {c} {d}");
+
+a = 9
+b = 1
+c = 5
+d = 2
+if a == b and c <= d:
+    print (f"was True for {a} {b} {c} {d}");
+else:
+    print (f"was False for {a} {b} {c} {d}");
+
+a = 0
+b = 0
+c = 0
+d = 0
+if True and a + b + c + d:
+    print (f"was True for {a} {b} {c} {d}");
+else:
+    print (f"was False for {a} {b} {c} {d}");
+
+a = 6
+b = 6
+if a != b:
+    print (f"was True for {a} {b}");
+else:
+    print (f"was False for {a} {b}");
+
+a = 2
+b = 1
+c = 0
+if a != b and a <= c or a <= b or False:
+    print(f"was True for {a} {b} {c}");
+else:
+    print (f"was False for {a} {b} {c}");
+
+a = 5
+b = 2
+c = 1
+if a != b and a <= c or a <= b or False:
+    print (f"was True for {a} {b} {c}");
+else:
+    print (f"was False for {a} {b} {c}");
+
+a = 7
+b = 0
+c = 4
+d = 3
+if b != 0 and d != 0 and a % b == 0 and c % d == 1:
+    print (f"was True for {a} {b} {c} {d}");
+else:
+    print (f"was False for {a} {b} {c} {d}");
+
+## End
+# I never lie (*_<)
